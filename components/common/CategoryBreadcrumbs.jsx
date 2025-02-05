dbbeb22e597648fc9dc271e6480ea989b10e185c
amanzaheer <amanzaheer0808@gmail.com>
2025-02-05 13:29:24 +0500

    category BreadCrumbs issue fix

--- components/common/CategoryBreadcrumbs.jsx
+++ components/common/CategoryBreadcrumbs.jsx
@@ -5,42 +5,24 @@ import React from "react";
 
 export default function CategoryBreadcrumbs({ breadcrumbs, className }) {
   const allBreadcrumbs = [
-    { label: "Category", url: "/" },
     breadcrumbs[breadcrumbs.length - 1]
   ];
 
   return (
     <div
       className={cn(
-        "w-full flex items-center  py-2 font-semibold text-gray-300",
+        "w-full flex items-center justify-center gap-2 py-3 font-semibold text-gray-300",
         className
       )}
     >
       {allBreadcrumbs.map((breadcrumb, index) => (
-        <span key={index} className="flex items-center gap-2">
-          {index > 0 && <ChevronsRight className="w-5" />}
-          {index === allBreadcrumbs.length - 1 ? (
-            <h1 className="text-primary1 ">
-              {breadcrumb.label
-                ?.replaceAll("%20", " ")
-                ?.replaceAll("%E2%80%99", "'")
-                ?.replaceAll("%E2%80%93", "-")}
-            </h1>
-          ) : (
-            <Link
-              title={breadcrumb.label
-                ?.replaceAll(" ", "-")
-                ?.replaceAll("%20", "-")
-                ?.replaceAll("%E2%80%99", "'")
-                ?.replaceAll("%E2%80%93", "-")}
-              href={breadcrumb.url}
-              className="transition-all "
-            >
-              {breadcrumb.label
-                ?.replaceAll("%20", " ")
-                ?.replaceAll("%E2%80%99", "'")}
-            </Link>
-          )}
+        <span key={index} className="flex items-center gap-3">
+          <h1 className="text-primary1 text-5xl font-extrabold">
+            {breadcrumb.label
+              ?.replaceAll("%20", " ")
+              ?.replaceAll("%E2%80%99", "'")
+              ?.replaceAll("%E2%80%93", "-")}
+          </h1>
         </span>
       ))}
     </div>
--- pages/[category]/index.jsx
+++ pages/[category]/index.jsx
@@ -114,8 +114,8 @@ export default function Category({
 
         <FullContainer className="py-16">
           <Container>
-            <Breadcrumbs breadcrumbs={breadcrumbs} className="mb-7 text-center " />
             <CategoryBreadcrumbs breadcrumbs={breadcrumbs} className="mb-7 text-center "/>
+            <Breadcrumbs breadcrumbs={breadcrumbs} className="mb-7 text-center " />
 
             <div className="grid grid-cols-1 md:grid-cols-home gap-12 w-full ">
               <div className="w-full grid grid-cols-1 gap-4 rounded-xl pt-6 ">
