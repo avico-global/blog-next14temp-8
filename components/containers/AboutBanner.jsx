6bc73afd8f8a3a9b646a450861277e47defceb63
amanzaheer <amanzaheer0808@gmail.com>
2025-05-08 23:11:47 +0500

    aout page h1 tag updated

--- components/containers/AboutBanner.jsx
+++ components/containers/AboutBanner.jsx
@@ -7,6 +7,7 @@ export default function AboutBanner({ image }) {
   return (
     <FullContainer className="relative">
       <Container className="gap-6 relative overflow-hidden h-[600px] text-white">
+        {/* Image container */}
         <div className="absolute inset-0 z-10">
           <Image
             src={image}
@@ -17,6 +18,12 @@ export default function AboutBanner({ image }) {
             loading="eager"
             className="w-full h-full object-cover"
           />
+        </div>
+        {/* Text container - separate from image */}
+        <div className="relative z-20">
+          <h1 className="font-extrabold text-6xl capitalize max-w-screen-md">
+            About Us
+          </h1>
         </div>
       </Container>
     </FullContainer>
